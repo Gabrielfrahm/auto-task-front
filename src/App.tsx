7b12d9227fd7303ab5719be1ac3c9daf02ea119f
Gabriel Frahm <gabriel_frahm@hotmail.com>
2024-02-21 21:57:55 -0300

    feat(save): save changes

--- src/App.tsx
+++ src/App.tsx
@@ -14,29 +14,24 @@ import {
 } from "@/components/ui/drawer"
 import Settings from "./components/form/settings";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "./components/ui/dialog";
+import {CheckSquare } from "lucide-react";
+
 
 function App() {
 
-  const [storage, setStorage] = useState();
+  const [storage, setStorage] = useState<Record<string, number>>();
 
-  const timerDurations: Record<string, number>  = {
-    pomodoro: storage ? storage['pomodoro'] * 60 : 25 * 60,
-    shortBreak: storage ? storage['shortBreak'] * 60 :  5 * 60,
-    longBreak: storage ? storage['longBreak'] * 60 : 10 * 60
-  };
+  const [timerDurations, setTimerDurations] = useState<Record<string, number>>({
+    pomodoro: storage ? storage['pomodoro'] *  60 : 25 * 60,
+    shortBreak:  storage ? storage['shortBreak'] *  60 : 5 * 60,
+    longBreak:  storage ? storage['longBreak'] *  60 : 10 * 60
+  });
 
-  // const [timerDurations, setTimerDurations] = useState<Record<string, number>>({
-  //   pomodoro: 25 * 60,
-  //   shortBreak: 5 * 60,
-  //   longBreak: 10 * 60
-  // });
   const [timerType, setTimerType] = useState<string>("pomodoro");
-  
+
   const [isActive, setIsActive] = useState<boolean>(false);
   const [seconds, setSeconds] = useState(timerDurations[timerType]);
   const [isOpenDialog, setIsOpenDialog]= useState<boolean>(false);
-
-  console.log(seconds)
 
   const handleType = (type: string) => {
     setTimerType(type);
@@ -63,20 +58,33 @@ function App() {
     if(!storage){
       setIsOpenDialog(true)
     }
+  }, [timerType]);
 
-  }, []);
+  useEffect(() => {
+    if(storage){
+      setTimerDurations({
+        pomodoro : storage['pomodoro'] *  60,
+        shortBreak: storage['shortBreak'] * 60,
+        longBreak: storage['longBreak'] * 60,
+      })
+      setSeconds(storage[timerType] * 60);
+    }
+  },[storage, timerType])
 
 
   return (
     <div className="bg-sky-950 h-dvh w-auto flex items-center justify-start flex-col p-10">
       <div className="flex items-center justify-between w-150">
-        <span>logo</span>
+        <div className="flex items-center gap-2">
+          <CheckSquare color="white" size={28} />
+          <p className="text-white font-bold" >Auto Task</p>
+        </div>
         <Drawer>
-          <DrawerTrigger className="text-white bg-black/[.08] hover:bg-black/[.5] w-32 rounded-md ">Settings</DrawerTrigger>
+          <DrawerTrigger className="text-white font-semibold bg-black/[.08] hover:bg-black/[.5] w-32 rounded-md ">Settings</DrawerTrigger>
             <DrawerContent className="bg-sky-950 text-white">
               <DrawerHeader>
                 <DrawerTitle className="text-center">Settings</DrawerTitle>
-                <Settings />
+                <Settings setSeconds={setSeconds} setTimerDurations={setTimerDurations} timerType={timerType} />
               </DrawerHeader>
               <DrawerFooter>
                 <DrawerClose>
@@ -89,24 +97,24 @@ function App() {
        <Separator className="my-4 w-150  bg-white/[.07]" />
       <div className="bg-white/[.07] h-96 w-128 rounded-md flex items-center justify-around flex-col">
         <div className="text-white flex justify-around items-center w-full">
-          <Button onClick={() => handleType("pomodoro")} 
+          <Button onClick={() => handleType("pomodoro")}
             className={`text-lg bg-black/[.08] hover:bg-black/[.5] ${timerType === 'pomodoro' ? 'bg-black' : ''}`}>
             Pomodoro
           </Button>
           <Button  onClick={() => handleType("shortBreak")}
            className={`text-lg bg-black/[.08] hover:bg-black/[.5] ${timerType === 'shortBreak' ? 'bg-black' : ''}`}>Short Break</Button>
-          <Button  onClick={() => handleType("longBreak")} 
+          <Button  onClick={() => handleType("longBreak")}
            className={`text-lg bg-black/[.08] hover:bg-black/[.5] ${timerType === 'longBreak' ? 'bg-black' : ''}`}>Long Break</Button>
         </div>
-        <div className="text-white mb-10"> 
-          <Timer 
-            isActive={isActive} 
-            seconds={seconds} 
-            setSeconds={setSeconds} 
+        <div className="text-white mb-10">
+          <Timer
+            isActive={isActive}
+            seconds={seconds}
+            setSeconds={setSeconds}
           />
         </div>
         <div className="flex items-center justify-around w-full">
-          <Button className="bg-zinc-50 w-52 h-16 text-2xl text-sky-950  hover:bg-black/[.5] hover:text-white shadow shadow-xl" 
+          <Button className="bg-zinc-50 w-52 h-16 text-2xl text-sky-950  hover:bg-black/[.5] hover:text-white shadow shadow-xl"
             onClick={toggle}>
               {isActive ? 'PAUSE' : 'START'}
           </Button>
@@ -118,7 +126,7 @@ function App() {
           <DialogHeader className="flex justify-center items-center">
             <DialogTitle className="text-white">Welcome!</DialogTitle>
             <DialogDescription>
-              <Settings setIsDialog={setIsOpenDialog} />
+              <Settings setIsDialog={setIsOpenDialog} setSeconds={setSeconds} setTimerDurations={setTimerDurations} timerType={timerType} />
             </DialogDescription>
           </DialogHeader>
         </DialogContent>
--- src/components/form/settings.tsx
+++ src/components/form/settings.tsx
@@ -4,6 +4,7 @@ import { Input } from "../ui/input";
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Button } from "../ui/button";
+import { useEffect } from "react";
 
 const formSchema = z.object({
   pomodoro: z.string(),
@@ -13,17 +14,20 @@ const formSchema = z.object({
 
 export type SettingsProps = {
     setIsDialog?: React.Dispatch<React.SetStateAction<boolean>>;
-    // setTimers:React.Dispatch<React.SetStateAction<Record<string, number>>>
+    setTimerDurations:React.Dispatch<React.SetStateAction<Record<string, number>>>;
+    setSeconds: React.Dispatch<React.SetStateAction<number>>;
+    timerType: string;
 }
 
 function Settings(props: Readonly<SettingsProps>){
+    const storage = localStorage.getItem("auto-task");
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
             pomodoro: "25",
             longBreak: "10",
             shortBreak: "5"
-          },
+        },
     });
 
     function onSubmit(values: z.infer<typeof formSchema>) {
@@ -36,15 +40,36 @@ function Settings(props: Readonly<SettingsProps>){
         if(!storage){
             localStorage.setItem("auto-task", JSON.stringify(values));
             storage = localStorage.getItem("auto-task");
+            if(storage){
+                props.setTimerDurations({
+                    pomodoro : JSON.parse(storage)['pomodoro'] *  60,
+                    shortBreak: JSON.parse(storage)['shortBreak'] * 60,
+                    longBreak: JSON.parse(storage)['longBreak'] *60,
+                  })
+                  props.setSeconds(JSON.parse(storage)[props.timerType] * 60);
+            }
         }else {
             localStorage.removeItem("auto-task");
             localStorage.setItem("auto-task", JSON.stringify(values));
             storage = localStorage.getItem("auto-task");
+            if(storage){
+                props.setTimerDurations({
+                    pomodoro : JSON.parse(storage)['pomodoro'] *  60,
+                    shortBreak: JSON.parse(storage)['shortBreak'] * 60,
+                    longBreak: JSON.parse(storage)['longBreak'] *60,
+                  })
+                  props.setSeconds(JSON.parse(storage)[props.timerType] * 60);
+            }
         }
-
-       
-        console.log(values)
     }
+
+    useEffect(() => {
+        if(storage) {
+            form.setValue("pomodoro",  JSON.parse(storage)['pomodoro']);
+            form.setValue("shortBreak",  JSON.parse(storage)['shortBreak']);
+            form.setValue("longBreak",  JSON.parse(storage)['longBreak']);
+        }
+    }, [storage, form])
 
     return (
         <Form {...form}>
